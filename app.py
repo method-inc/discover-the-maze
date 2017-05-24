242507804612687286e394a953a0680d3ca9efc5
Rob Florence <rflorence@skookum.com>
2017-05-24 10:55:45 -0400

    bind flask to $PORT if applicable

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ def upload_maze():
     return jsonify({ hello: 'world' })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
